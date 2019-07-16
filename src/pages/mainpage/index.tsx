6e2f80d2fc1912329528c5ed2cf39bf2d58241f6
akari0624 <akari0624@hotmail.com>
2019-07-16 18:05:49 +0800

    apply a green desk and put a deck

--- src/pages/mainpage/index.tsx
+++ src/pages/mainpage/index.tsx
@@ -1,6 +1,33 @@
 import React from 'react'
+import Styled from 'styled-components'
 import CardDeck from './containers/CardDeck'
+import GreenTable from '../../../assets/green_felt.jpg'
+
+const MainTable = Styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${GreenTable});
+  overflow: hidden;
+`
+
+type DeckProps = {
+  marginLeft: string
+  marginTop: string
+}
+
+const CardDockWrapper = Styled.section<DeckProps>`
+  margin-left:${(props) => (props.marginLeft)};
+  margin-top:${(props) => (props.marginTop)};
+`
+
 
 interface Props {}
 
-export default (props: Props) => <CardDeck />
+export default (props: Props) => (
+  <MainTable>
+    <CardDockWrapper marginLeft="300px" marginTop="500px">
+      <CardDeck />
+    </CardDockWrapper>
+
+  </MainTable>
+)
