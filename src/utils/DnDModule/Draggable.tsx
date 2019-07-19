ec14c989df41061c8de22237dcd10986b08919a2
Morris-Chen <akari0624@hotmail.com>
2019-07-20 01:55:13 +0800

    用React.Children.. React.cloneElement 這樣的api克服掉必須再加一層div的問題

--- src/utils/DnDModule/Draggable.tsx
+++ src/utils/DnDModule/Draggable.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react'
 import { DnDModuleConstants } from './constant'
 interface Props {
+  key: string
   draggableItemId: string
   belongDroppableId: string
   children: JSX.Element
@@ -8,19 +9,23 @@ interface Props {
   isDraggable?: boolean
 }
 
-export default function Draggable(props: Props) {
-  const { draggableItemId, belongDroppableId, index, isDraggable = true } = props
+// the `any` return type is necessary @see https://github.com/DefinitelyTyped/DefinitelyTyped/issues/27805#issuecomment-508106043 
+export default function Draggable(props: Props): any {
+  const { draggableItemId, belongDroppableId, index, isDraggable = true, key } = props
   const onDragStartHandler = useCallback(
     (e: React.DragEvent<HTMLElement>) => {
       e.dataTransfer.setData(DnDModuleConstants.dragItemId, draggableItemId)
       e.dataTransfer.setData(DnDModuleConstants.belongDroppableId, belongDroppableId)
       e.dataTransfer.setData(DnDModuleConstants.draggedItemIndex, String(index))
     },
-    [draggableItemId, belongDroppableId, index, isDraggable]
+    [draggableItemId, belongDroppableId, index,]
   )
-  return (
-    <div draggable={isDraggable} onDragStart={onDragStartHandler}>
-      {props.children}
-    </div>
+
+  return React.Children.map(props.children, childVDom =>
+    React.cloneElement(childVDom, {
+      draggable: isDraggable,
+      onDragStart: onDragStartHandler,
+      key: key,
+    })
   )
 }
