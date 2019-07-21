e0fe8f935641a3401b61b8b317eb725fe2a367dd
Morris-Chen <akari0624@hotmail.com>
2019-07-22 02:08:07 +0800

    refactor: change function name

--- src/game_logic/__test__/index.test.ts
+++ src/game_logic/__test__/index.test.ts
@@ -1,10 +1,10 @@
-import { getCards } from '../index'
+import { getAllCards } from '../index'
 import { cleanup } from '@testing-library/react'
 
 afterEach(cleanup)
 
 describe('can get correct card number array', () => {
-  const cards = getCards(30)
+  const cards = getAllCards(30)
   test('there has 52 cards', () => {
     expect(cards.length).toBe(52)
   })
