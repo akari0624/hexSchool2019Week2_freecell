a1abd938ed1c4f030a2194e7b831a7818da2de50
Morris-Chen <akari0624@hotmail.com>
2019-07-21 22:39:27 +0800

    change test rule

--- src/game_logic/__test__/index.test.ts
+++ src/game_logic/__test__/index.test.ts
@@ -8,4 +8,11 @@ describe('can get correct card number array', () => {
   test('there has 52 cards', () => {
     expect(cards.length).toBe(52)
   })
+
+  test('the number is start from 0, not 1', () => {
+    expect(cards.includes(0)).toBe(true)
+    expect(cards.includes(1)).toBe(true)
+    expect(cards.includes(51)).toBe(true)
+    expect(cards.includes(52)).toBe(false)
+  })
 })
